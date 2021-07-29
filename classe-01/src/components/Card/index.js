084526359771fd34715d38a75557f4945cbe8f99
estefaneluz <estefanecortes@gmail.com>
2021-07-29 20:04:05 -0300

    card estilizado

--- classe-01/src/components/Card/index.js
+++ classe-01/src/components/Card/index.js
@@ -1,35 +1,11 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-}));
+import ListItemText from '@material-ui/core/ListItemText';
+import { useStyles } from './style';
 
 export default function RecipeReviewCard() {
   const classes = useStyles();
@@ -37,24 +13,19 @@ export default function RecipeReviewCard() {
   return (
     <Card className={classes.root}>
       <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            R
-          </Avatar>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title="Pikachu"
       />
       <CardMedia
         className={classes.media}
-        image="/static/images/cards/paella.jpg"
-        title="Paella dish"
+        image=""
+        title="Pikachu"
       />
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+        <Typography variant="h6" component="h2">
+            Abilities
         </Typography>
+        <ListItemText className={classes.list} primary={"Habilidade 1"} />
+        <ListItemText className={classes.list} primary={"Habilidade 2"} />
       </CardContent>
     </Card>
   );
