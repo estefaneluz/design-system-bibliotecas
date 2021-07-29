7fe9b58b5d65e85c709266376ea4bc1a0caf5cb0
estefaneluz <estefanecortes@gmail.com>
2021-07-29 19:42:27 -0300

    consertando importacao

--- classe-01/src/App.js
+++ classe-01/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Card from './components/Card'
+import Search from './components/Search'
+import Navbar from './components/Navbar'
+import Button from './components/Button'
 
 function App() {
   return (
     <div className="app">
-      
+      <Card/>
     </div>
   );
 }
