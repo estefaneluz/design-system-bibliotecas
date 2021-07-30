e2b2490926313dfdc92c212b5ead707b6f3dc397
estefaneluz <estefanecortes@gmail.com>
2021-07-29 21:12:24 -0300

    integrando com a PokeAPI

--- classe-01/src/App.js
+++ classe-01/src/App.js
@@ -2,8 +2,33 @@ import './App.css';
 import Card from './components/Card'
 import Search from './components/Search'
 import Navbar from './components/Navbar'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    pesquisarPokemon('pikachu');
+  }, []);
+
+  async function pesquisarPokemon(search){
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search.toLowerCase()}`);
+
+      const { abilities, name } = await response.json();
+
+      setPokemon({
+        abilities: abilities,
+        name: name
+      });
+
+      console.log(pokemon.name); 
+    } 
+    catch (error) {
+      return error.message;
+    }
+  }
+
   return (
     <>
       <Navbar/>
